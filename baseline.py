8c8e69d23c2d0ca60b52f2d7e34a161d8f8a0580
dan11109 <steved7@rpi.edu>
2022-04-01 11:36:13 -0400

    baseline

---
+++ baseline.py
@@ -0,0 +1,173 @@
+from sklearn.cluster import DBSCAN
+import numpy as np
+import json 
+import pickle
+import math
+import re
+import random
+from collections import Counter
+from build_index import Index
+from scipy import spatial
+
+
+# ssh steved7@acidburn.cs.rpi.edu
+
+
+def KL(vec1, vec2):
+	p = np.asarray(vec1)
+	q = np.asarray(vec2)
+	p  = p  / np.linalg.norm(p )
+	q = q / np.linalg.norm(q)
+	epsilon = 0.00001
+	p = p+epsilon
+	q = q+epsilon
+	divergence = np.sum(p*np.log(p/q))
+	return divergence 
+
+
+
+
+
+f = open('data/info.json')
+info = json.load(f)
+
+with open('all_data.pkl', 'rb') as inp:
+    inn = pickle.load(inp)
+
+with open('embeddings.pkl', "rb") as fIn:
+	    stored_data = pickle.load(fIn)
+	    stored_order = stored_data['order']
+	    stored_embeddings = stored_data['embeddings']
+
+
+for i in range(2000 - 500):#filter out 
+	idx = random.randrange(0,len(stored_order))
+	stored_order.pop(idx)
+	stored_embeddings.pop(idx)
+
+
+clustering = DBSCAN(eps=.30, min_samples=10, metric='cosine').fit(stored_embeddings)
+
+
+lst = clustering.labels_
+
+#print(clustering.labels_)
+print(max(clustering.labels_))
+print(np.count_nonzero(clustering.labels_ != -1))
+print(np.count_nonzero(clustering.core_sample_indices_ != -1))
+	
+
+
+clusters = {}
+
+for i in range(len(clustering.labels_)):
+	temp = clustering.labels_[i]
+	if(temp != -1):
+		if(temp in clusters.keys()):
+			clusters[temp].append(stored_order[i])
+		else:
+			clusters[temp] = [stored_order[i]]
+
+
+
+
+tfidf = {}
+for word in inn.doc_sim_score.keys(): 
+	
+	count = 0
+	for doc in inn.doc_sim_score[word]:
+		if(doc[1] != 0):
+			count += 1
+
+	if(count <= 1): #filter out single words 
+		continue
+
+	for doc in inn.doc_sim_score[word]:
+		if(doc[0] in tfidf.keys()): 
+			tfidf[doc[0]].append(doc[1])
+		else:
+			tfidf[doc[0]]= [doc[1]]
+
+
+
+##########COSINE##############
+# [(d1,d1)] = cos#
+set_cos = set()
+d = {}
+number = 0
+clusters_cos = []
+
+for i in clusters.keys():
+
+	tmp = clusters[i].copy()
+	while(len(tmp) > 1):
+
+		idx = random.randrange(0,len(tmp))
+		cent = tmp.pop(idx)
+		j = 0
+		first = True
+		while(j < len(tmp)):
+			dist = (1-spatial.distance.cosine(tfidf[cent], tfidf[tmp[j]] ))
+			if(dist > .8):
+				d[(cent,tmp[j])] = dist
+				set_cos.add(tmp[j])
+				tmp.pop(j)
+				if(first):
+					clusters_cos.append([cent])
+					set_cos.add(cent)
+					number += 1
+				first = False
+				clusters_cos[-1].append(tmp[j])
+				number+=1
+
+			else:
+				j+=1
+
+print("Number of articles flagged (cos): " + str(number))
+
+for i in d.keys():
+	print(i,end=': ')
+	print(d[i])
+
+print()
+for i in clusters_cos:	
+	print(i)
+
+
+##############KL##############
+# [(d1,d1)] = cos#
+d = {}
+number = 0
+set_kl = set()
+for i in clusters.keys():
+
+	tmp = clusters[i].copy()
+	while(len(tmp) > 1):
+
+		idx = random.randrange(0,len(tmp))
+		cent = tmp.pop(idx)
+		j = 0
+		first = True
+		while(j < len(tmp)):
+			dist = KL(tfidf[cent], tfidf[tmp[j]])
+			if(dist < 4.0):
+				d[(cent,tmp[j])] = dist
+				set_kl.add(tmp[j])
+				tmp.pop(j)
+				if(first):
+					set_kl.add(cent)
+					number += 1
+				first = False
+				number+=1
+
+			else:
+				j+=1
+
+print("Number of articles flagged (kl): " + str(number))
+print("Number in commom KL and Cosine: " + str(len(set_cos.intersection(set_kl))))
+
+
+for i in d.keys():
+	print(i,end=': ')
+	print(d[i])
+			
