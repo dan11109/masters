0277a5d622940683d82e5f10dd1fc8ae4eafb832
dan11109 <steved7@rpi.edu>
2022-04-20 12:49:15 -0400

    common

--- all_methods.py
+++ all_methods.py
@@ -98,7 +98,7 @@ for word in inn.doc_sim_score.keys():
 ##### baseline cosine tf-idf #####
 number = 0
 base_clust = []
-base_set = set()
+set1 = set()
 start = time.time()
 tmp = stored_order.copy()
 while(len(tmp) > 1):
@@ -112,11 +112,11 @@ while(len(tmp) > 1):
 		if(dist > .8):
 			if(first):
 				base_clust.append([cent])
-				base_set.add(cent)
+				set1.add(cent)
 				number += 1
 			first = False
 			base_clust[-1].append(tmp[j])
-			base_set.add(tmp[j])
+			set1.add(tmp[j])
 			tmp.pop(j)
 			number+=1
 
@@ -128,7 +128,7 @@ print("Baseline cosine tf-idf: " + str(end - start))
 print("Number of articles flagged (BASELINE): " + str(number))
 
 sources = set()
-for i in base_set:
+for i in set1:
 	sources.add(info[i]['source'])
 
 print("Number of sources (BASELINE): ", end = '')
@@ -142,7 +142,7 @@ print(len(base_clust))
 ##### baseline KL tf-idf #####
 number = 0
 base_clust = []
-base_set = set()
+set2 = set()
 start = time.time()
 tmp = stored_order.copy()
 while(len(tmp) > 1):
@@ -156,11 +156,11 @@ while(len(tmp) > 1):
 		if(dist < 10.0):
 			if(first):
 				base_clust.append([cent])
-				base_set.add(cent)
+				set2.add(cent)
 				number += 1
 			first = False
 			base_clust[-1].append(tmp[j])
-			base_set.add(tmp[j])
+			set2.add(tmp[j])
 			tmp.pop(j)
 			number+=1
 
@@ -173,7 +173,7 @@ print("Baseline KL tf-idf: " + str(end - start))
 print("Number of articles flagged (BASELINE): " + str(number))
 
 sources = set()
-for i in base_set:
+for i in set2:
 	sources.add(info[i]['source'])
 
 print("Number of sources (BASELINE): ", end = '')
@@ -186,7 +186,7 @@ print(len(base_clust))
 ##### baseline cosine bert #####
 number = 0
 base_clust = []
-base_set = set()
+set3 = set()
 start = time.time()
 tmp = stored_order.copy()
 while(len(tmp) > 1):
@@ -200,11 +200,11 @@ while(len(tmp) > 1):
 		if(dist > .8):
 			if(first):
 				base_clust.append([cent])
-				base_set.add(cent)
+				set3.add(cent)
 				number += 1
 			first = False
 			base_clust[-1].append(tmp[j])
-			base_set.add(tmp[j])
+			set3.add(tmp[j])
 			tmp.pop(j)
 			number+=1
 
@@ -217,7 +217,7 @@ print("Baseline cosine BERT: " + str(end - start))
 print("Number of articles flagged (BASELINE): " + str(number))
 
 sources = set()
-for i in base_set:
+for i in set3:
 	sources.add(info[i]['source'])
 
 print("Number of sources (BASELINE): ", end = '')
@@ -229,7 +229,7 @@ print(len(base_clust))
 ##### baseline KL bert #####
 number = 0
 base_clust = []
-base_set = set()
+set4 = set()
 start = time.time()
 tmp = stored_order.copy()
 while(len(tmp) > 1):
@@ -243,11 +243,11 @@ while(len(tmp) > 1):
 		if(dist < 10.0):
 			if(first):
 				base_clust.append([cent])
-				base_set.add(cent)
+				set4.add(cent)
 				number += 1
 			first = False
 			base_clust[-1].append(tmp[j])
-			base_set.add(tmp[j])
+			set4.add(tmp[j])
 			tmp.pop(j)
 			number+=1
 
@@ -260,7 +260,7 @@ print("Baseline KL BERT: " + str(end - start))
 print("Number of articles flagged (BASELINE): " + str(number))
 
 sources = set()
-for i in base_set:
+for i in set4:
 	sources.add(info[i]['source'])
 
 print("Number of sources (BASELINE): ", end = '')
@@ -272,7 +272,7 @@ print(len(base_clust))
 
 ##### cluster bert, cosine tf-idf ##### 
 start = time.time()
-set_cos = set()
+set5 = set()
 d = {}
 number = 0
 clusters_cos = []
@@ -288,10 +288,10 @@ for i in clusters.keys():
 			dist = (1-spatial.distance.cosine(tfidf[cent], tfidf[tmp[j]] ))
 			if(dist > .8):
 				d[(cent,tmp[j])] = dist
-				set_cos.add(tmp[j])
+				set5.add(tmp[j])
 				if(first):
 					clusters_cos.append([cent])
-					set_cos.add(cent)
+					set5.add(cent)
 					number += 1
 				first = False
 				clusters_cos[-1].append(tmp[j])
@@ -306,7 +306,7 @@ print("Time for clustering with BERT, and Cosine TF-IDF: " + str(end - start))
 print("Number of articles flagged (cos): " + str(number))
 
 sources = set()
-for i in set_cos:
+for i in set5:
 	sources.add(info[i]['source'])
 
 print("Number of sources (COS): ", end = '')
@@ -320,7 +320,7 @@ start = time.time()
 counter = 0
 d = {}
 number = 0
-set_kl = set()
+set6 = set()
 for i in clusters.keys():
 
 	tmp = clusters[i].copy()
@@ -334,10 +334,10 @@ for i in clusters.keys():
 			dist = KL(tfidf[cent], tfidf[tmp[j]])
 			if(dist < 10.0):
 				d[(cent,tmp[j])] = dist
-				set_kl.add(tmp[j])
+				set6.add(tmp[j])
 				tmp.pop(j)
 				if(first):
-					set_kl.add(cent)
+					set6.add(cent)
 					number += 1
 					counter += 1
 				first = False
@@ -350,10 +350,9 @@ end = time.time()
 print()
 print("Time for clustering with BERT, and KL TF-IDF: " + str(end - start))
 print("Number of articles flagged (kl): " + str(number))
-print("Number in commom KL and Cosine: " + str(len(set_cos.intersection(set_kl))))
 	
 sources = set()
-for i in set_kl:
+for i in set6:
 	sources.add(info[i]['source'])
 
 print("Number of sources (KL): ", end = '')
@@ -365,7 +364,7 @@ print(counter)
 
 ##### cluster bert, cosine bert #####
 start = time.time()
-set_cos = set()
+set7 = set()
 d = {}
 number = 0
 clusters_cos = []
@@ -381,10 +380,10 @@ for i in clusters.keys():
 			dist = (1-spatial.distance.cosine(embedded_dict[cent], embedded_dict[tmp[j]] ))
 			if(dist > .8):
 				d[(cent,tmp[j])] = dist
-				set_cos.add(tmp[j])
+				set7.add(tmp[j])
 				if(first):
 					clusters_cos.append([cent])
-					set_cos.add(cent)
+					set7.add(cent)
 					number += 1
 				first = False
 				clusters_cos[-1].append(tmp[j])
@@ -399,7 +398,7 @@ print("Time for clustering with BERT, and Cosine BERT: " + str(end - start))
 print("Number of articles flagged (cos): " + str(number))
 
 sources = set()
-for i in set_cos:
+for i in set7:
 	sources.add(info[i]['source'])
 
 print("Number of sources (COS): ", end = '')
@@ -416,7 +415,7 @@ start = time.time()
 counter = 0
 d = {}
 number = 0
-set_kl = set()
+set8 = set()
 for i in clusters.keys():
 
 	tmp = clusters[i].copy()
@@ -430,10 +429,10 @@ for i in clusters.keys():
 			dist = KL(embedded_dict[cent], embedded_dict[tmp[j]])
 			if(dist < 10.0):
 				d[(cent,tmp[j])] = dist
-				set_kl.add(tmp[j])
+				set8.add(tmp[j])
 				tmp.pop(j)
 				if(first):
-					set_kl.add(cent)
+					set8.add(cent)
 					number += 1
 					counter += 1
 				first = False
@@ -446,10 +445,9 @@ end = time.time()
 print()
 print("Time for clustering with BERT, and KL BERT: " + str(end - start))
 print("Number of articles flagged (kl): " + str(number))
-print("Number in commom KL and Cosine: " + str(len(set_cos.intersection(set_kl))))
 	
 sources = set()
-for i in set_kl:
+for i in set8:
 	sources.add(info[i]['source'])
 
 print("Number of sources (KL): ", end = '')
@@ -461,6 +459,85 @@ print(counter)
 
 
 
+print("Articles in common: ")
+print()
+print('Baseline cosine tf-idf:')
+print(str(len(set1.intersection(set2))))
+print(str(len(set1.intersection(set3))))
+print(str(len(set1.intersection(set4))))
+print(str(len(set1.intersection(set5))))
+print(str(len(set1.intersection(set6))))
+print(str(len(set1.intersection(set7))))
+print(str(len(set1.intersection(set8))))
 
+print()
+print('Baseline KL tf-idf:')
+print(str(len(set2.intersection(set1))))
+print(str(len(set2.intersection(set3))))
+print(str(len(set2.intersection(set4))))
+print(str(len(set2.intersection(set5))))
+print(str(len(set2.intersection(set6))))
+print(str(len(set2.intersection(set7))))
+print(str(len(set2.intersection(set8))))
+
+print()
+print('Baseline cosine BERT:')
+print(str(len(set3.intersection(set1))))
+print(str(len(set3.intersection(set2))))
+print(str(len(set3.intersection(set4))))
+print(str(len(set3.intersection(set5))))
+print(str(len(set3.intersection(set6))))
+print(str(len(set3.intersection(set7))))
+print(str(len(set3.intersection(set8))))
+
+print()
+print('Baseline KL BERT:')
+print(str(len(set4.intersection(set1))))
+print(str(len(set4.intersection(set2))))
+print(str(len(set4.intersection(set3))))
+print(str(len(set4.intersection(set5))))
+print(str(len(set4.intersection(set6))))
+print(str(len(set4.intersection(set7))))
+print(str(len(set4.intersection(set8))))
+
+print()
+print('Clustering with BERT, and Cosine TF-IDF:')
+print(str(len(set5.intersection(set1))))
+print(str(len(set5.intersection(set2))))
+print(str(len(set5.intersection(set3))))
+print(str(len(set5.intersection(set4))))
+print(str(len(set5.intersection(set6))))
+print(str(len(set5.intersection(set7))))
+print(str(len(set5.intersection(set8))))
+
+print()
+print('Clustering with BERT, and KL TF-IDF:')
+print(str(len(set6.intersection(set1))))
+print(str(len(set6.intersection(set2))))
+print(str(len(set6.intersection(set3))))
+print(str(len(set6.intersection(set4))))
+print(str(len(set6.intersection(set5))))
+print(str(len(set6.intersection(set7))))
+print(str(len(set6.intersection(set8))))
+
+print()
+print('Clustering with BERT, and Cosine BERT:')
+print(str(len(set7.intersection(set1))))
+print(str(len(set7.intersection(set2))))
+print(str(len(set7.intersection(set3))))
+print(str(len(set7.intersection(set4))))
+print(str(len(set7.intersection(set5))))
+print(str(len(set7.intersection(set6))))
+print(str(len(set7.intersection(set8))))
+
+print()
+print('Clustering with BERT, and KL BERT:')
+print(str(len(set8.intersection(set1))))
+print(str(len(set8.intersection(set2))))
+print(str(len(set8.intersection(set3))))
+print(str(len(set8.intersection(set4))))
+print(str(len(set8.intersection(set5))))
+print(str(len(set8.intersection(set6))))
+print(str(len(set8.intersection(set7))))
 
 
