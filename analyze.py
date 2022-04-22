3d2db9c9f9ef4358f8d20053a31a8ce26275c5c6
dan11109 <steved7@rpi.edu>
2022-04-22 18:02:49 -0400

    small fixes:

--- analyze.py
+++ analyze.py
@@ -30,16 +30,56 @@ tmp = lsts[0].intersection(lsts[2])
 
 only_bert = lsts[2] - tmp
 
-'''
-for i in only_bert:
 
-    print(info[i]['title'])
-    print(info[i]['url'])
-    print()
-'''
 print("Clusters found in Cosine BERT but not in Cosine tfidf:")
 
+with open('embeddings.pkl', "rb") as fIn:
+        stored_data = pickle.load(fIn)
+        stored_order = stored_data['order']
+        stored_embeddings = stored_data['embeddings']
 
+
+embedded_dict = {}
+temp_order = []
+temp_embeddings = []
+
+for i in range(2000):  
+    embedded_dict[stored_order[i]] = stored_embeddings[i]
+    temp_order.append(stored_order[i])
+    temp_embeddings.append(stored_embeddings[i])
+
+
+
+only_bert_clust = []
+cosines = []
+
+for i in clusters[1]:
+    tmp = []
+    count = 0
+    for j in i:
+        if(j in only_bert):
+            count += 1
+            tmp.append(j)
+
+    if(count > 1):
+         only_bert_clust.append(tmp)
+
+for i in only_bert_clust:
+    max_cos = 0.0
+    for j in range(len(i)-1):
+        d = (1-spatial.distance.cosine(embedded_dict[i[0]], embedded_dict[i[j+1]] ))
+        if(d > max_cos):
+            max_cos = d
+
+    cosines.append((max_cos,i))
+
+for i in sorted(cosines):
+    print(i)
+
+
+
+
+'''
 out_string = ''
 for i in clusters[1]:
     tmp = ''
@@ -53,6 +93,6 @@ for i in clusters[1]:
             
 print(out_string)       
 
-
+'''
 
 
